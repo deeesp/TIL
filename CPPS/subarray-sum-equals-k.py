bba71bf9b69bce539c6691fd096e2b7e02ede4cf
JINWOO OH <48049847+DLOHai@users.noreply.github.com>
2019-08-10 12:23:05 +0900

    Update subarray-sum-equals-k.py

--- CPPS/subarray-sum-equals-k.py
+++ CPPS/subarray-sum-equals-k.py
@@ -20,14 +20,7 @@ class Solution:
     Space: O(N)
     '''
     def subarraySum(self, nums: List[int], k: int) -> int:
-        '''
-        연속 부분집합의 합이 k인 부분집합의 개수
-        부분집합의 길이가 미정
-        길이가 1이어도 되나?
-        될거야
-        
-        [1,3,5,7,8,9]  15
-        '''
+
         count, cur, res = {0: 1}, 0, 0
         for num in nums:
             cur += num
