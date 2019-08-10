4c1c01863c0ca8b47f633fa22f287bb7cceda277
JINWOO OH <48049847+DLOHai@users.noreply.github.com>
2019-08-10 12:22:40 +0900

    Update subarray-sum-equals-k.py

--- CPPS/subarray-sum-equals-k.py
+++ CPPS/subarray-sum-equals-k.py
@@ -15,5 +15,22 @@ The range of numbers in the array is [-1000, 1000] and the range of the integer 
 """
 
 class Solution:
-    def palindromePairs(self, words: List[str]) -> List[List[int]]:
+    '''
+    Time: O(N)
+    Space: O(N)
+    '''
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        '''
+        연속 부분집합의 합이 k인 부분집합의 개수
+        부분집합의 길이가 미정
+        길이가 1이어도 되나?
+        될거야
         
+        [1,3,5,7,8,9]  15
+        '''
+        count, cur, res = {0: 1}, 0, 0
+        for num in nums:
+            cur += num
+            res += count.get(cur - k, 0)
+            count[cur] = count.get(cur, 0) + 1
+        return res
