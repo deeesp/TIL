c7cba726776f8d4622ec4eafd925f9f342633c66
JINWOO OH <48049847+DLOHai@users.noreply.github.com>
2019-10-07 02:56:22 +0900

    Update dijkstra.py typo

--- dijkstra.py
+++ dijkstra.py
@@ -5,7 +5,7 @@ def dijkstra(costs, graph, parents, processed):
         lowest_cost_node = None
         for node in costs:
             cost = costs[node]
-            if cost < lowest_cost_node and node not in processed:
+            if cost < lowest_cost and node not in processed:
                 lowest_cost = cost
                 lowest_cost_node = node
         return lowest_cost_node
